2f963eb4c5d28f3333d52c199aad126194db8fcf
Glitch (glitch-hello-website) <none>
2021-06-26 23:41:16 +0000

    📮🍕 Checkpoint ./index.html:31031725/84 ./script.js:31031725/612

--- script.js
+++ script.js
@@ -66,7 +66,20 @@ return d.toFixed(5); //Retorna tres decimales
 console.log(getKilometros(lat1,long1,lat2,long2))
 
 
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
 
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate.toLocaleDateString(), '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
 
 
